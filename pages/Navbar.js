f7e81c450b24f330cbcba8f4f35eaec3af15f3ca
nishanholla <hollanishan@gmail.com>
2024-09-19 17:50:30 +0530

    minor bug fixes

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import logo from "../public/mylogo.png"
-import Image from "next/image";
+import Image from 'next/image';
+import logo from '../public/mylogo.png';
 
 const navItems = [
   { name: 'Home', link: '#home' },
@@ -14,7 +14,6 @@ const navItems = [
 ];
 
 const Navbar = () => {
-  const router = useRouter();
   const [activeSection, setActiveSection] = useState('');
 
   useEffect(() => {
@@ -23,43 +22,49 @@ const Navbar = () => {
       sections.forEach((section) => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.offsetHeight;
-        if (window.pageYOffset >= sectionTop - sectionHeight / 2) {
+        if (window.pageYOffset >= sectionTop - sectionHeight / 2 &&
+            window.pageYOffset < sectionTop + sectionHeight) {
           setActiveSection(`#${section.id}`);
         }
       });
     };
 
-    document.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // Call on initial render
     return () => {
-      document.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
   return (
-    <nav className="py-10 ease-in-out 0.3s  mb-12 lg:flex justify-between dark:text-gray-900 lg:py-4 lg:px-12">
-      <a href='#'><div className="hover:animate-spin lg:flex-grow h-20 w-20 block mt-2 lg:inline-block lg:mt-0  py-2 rounded  mr-2 text-left fixed"><button><Image src={logo} className="scale-100" /></button></div></a>
+    <nav className="py-10 ease-in-out 0.3s mb-12 lg:flex justify-between dark:text-gray-900 lg:py-4 lg:px-12">
+      <a href="#home">
+        <div className="hover:animate-spin lg:flex-grow h-20 w-20 block mt-2 lg:inline-block lg:mt-0 py-2 rounded mr-2 text-left fixed">
+          <button>
+            <Image src={logo} className="scale-100" alt="Logo" />
+          </button>
+        </div>
+      </a>
       <div className='text-md text-center font-bold text-white lg:flex-grow lg:mr-20 lg:mt-5'>
-        <ul className="  text-right ml-20 justify-center items-center h-16">
+        <ul className="text-right ml-20 flex justify-center items-center h-16">
           {navItems.map((item) => (
-            <li key={item.link} className="block  lg:inline-block hover:text-white px-4 py-2 rounded hover:bg-blue-900 lg:mr-2">
-              <Link href={`/${item.link}`}>
-                <a
-                  className={`${activeSection === item.link ? 'text-yellow-500' : ''
-                    }`}
-                >
-                  {item.name}
-                </a>
-              </Link>
+            <li key={item.link} className="block lg:inline-block hover:text-white px-4 py-2 rounded hover:bg-blue-900 lg:mr-2">
+              <a
+                href={item.link}
+                className={`${activeSection === item.link ? 'text-yellow-500' : ''}`}
+              >
+                {item.name}
+              </a>
             </li>
           ))}
         </ul>
       </div>
       <ul className="flex items-center">
-
         <li>
           <a
-            className="bg-gradient-to-r from-blue-900 text- to-teal-500 text-white  px-4 py-2 border-none rounded-md ml-20"
-            href="https://drive.google.com/file/d/1iXHN86s_C6Pi6S2IkRE2pJMwe8eQXlT6/view?usp=drive_link" download="NishanHolla_Resume.pdf"
+            className="bg-gradient-to-r from-blue-900 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-20"
+            href="https://drive.google.com/file/d/1iXHN86s_C6Pi6S2IkRE2pJMwe8eQXlT6/view?usp=drive_link"
+            download="NishanHolla_Resume.pdf"
           >
             Resume
           </a>
--- pages/experience.js
+++ pages/experience.js
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
-import AOS from 'aos'
+import AOS from 'aos';
 
 export default function Experience() {
   useEffect(() => {
-    AOS.init({
-    });
+    AOS.init({});
   }, []);
 
   return (
     <div>
-      <h3 data-aos="slide-up" className="animate-pulse text-5xl ml-20 dark:text-teal-600 md:text-center">EXPERIENCE AND QUALIFICATIONS</h3><br /><br /><br /><br />
+      <h3 data-aos="slide-up" className="animate-pulse text-5xl ml-20 dark:text-teal-600 md:text-center">
+        EXPERIENCE AND QUALIFICATIONS
+      </h3><br /><br /><br /><br />
 
       <div data-aos="slide-up" className="grid lg:grid-cols-2 md:grid-cols-2 gap-11 lg:pr-20 mx-20">
         {/* Education */}
@@ -42,19 +43,19 @@ export default function Experience() {
           <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
             <h6 className="mr-10">☁️<span className="lg:ml-10 ml-16 text-center">CivicCraft Studio</span></h6>
             <p className="ml-16">Software Development Intern</p>
-            <p className="text-center">Jul'24 - Present</p>
+            <p className="text-center">Jul&apos;24 - Present</p>
           </div><br /><br />
           {/* CoDMAV Research Center */}
           <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
             <h6 className="mr-10">☁️<span className="lg:ml-10 ml-16 text-center">Codmav Research Center</span></h6>
             <p className="ml-16">Research Intern</p>
-            <p className="text-center">Jun'24 - Jul'24</p>
+            <p className="text-center">Jun&apos;24 - Jul&apos;24</p>
           </div><br /><br />
           {/* LetsEndorse Mobile App */}
           <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
             <h6 className="mr-10">☁️<span className="lg:ml-10 ml-16 text-center">LetsEndorse Mobile App</span></h6>
             <p className="ml-16">Mobile App Intern</p>
-            <p className="text-center">Feb'24 - Mar'24</p>
+            <p className="text-center">Feb&apos;24 - Mar&apos;24</p>
           </div>
         </div>
       </div>
@@ -63,10 +64,14 @@ export default function Experience() {
       <div className="from-teal-500 w-50 h-30 hover:bg-gray-900 mx-auto mt-10 text-center">
         <h4 className="fontt text-teal-500 text-center text-3xl">AWARDS AND ACHIEVEMENTS</h4><br />
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
-          <p>🥇 1st Place, inGenius 12.0 Hackathon - InGenius Community<br />Worked on improving supply chain and information retrieval for medicine tracking (2024)</p><br />
-          <p>🥇 1st Place, Kalpana Hack For Humanity - IEEE Society<br />Developed a Mental Health Wellbeing App using Embeddings and Langchain (2024)</p><br />
-          <p>🏅 Top 10, Kodikon 3.0 National Level Hackathon - Club Embrione<br />Researched and built AR tech using Machine Learning (2023)</p><br />
-          <p>🏅 Ranked #15 in Open Source Contributions Runner Up - ACM Hacktober Fest IEEE Society<br />Closed multiple issues in open source projects (2023)</p>
+          <p>🥇 1st Place, inGenius 12.0 Hackathon - InGenius Community<br />
+            Worked on improving supply chain and information retrieval for medicine tracking (2024)</p><br />
+          <p>🥇 1st Place, Kalpana Hack For Humanity - IEEE Society<br />
+            Developed a Mental Health Wellbeing App using Embeddings and Langchain (2024)</p><br />
+          <p>🏅 Top 10, Kodikon 3.0 National Level Hackathon - Club Embrione<br />
+            Researched and built AR tech using Machine Learning (2023)</p><br />
+          <p>🏅 Ranked #15 in Open Source Contributions Runner Up - ACM Hacktober Fest IEEE Society<br />
+            Closed multiple issues in open source projects (2023)</p>
         </div>
       </div>
     </div>
